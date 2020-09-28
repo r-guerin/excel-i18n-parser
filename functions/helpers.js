37968cc30e58a9ac2724ca7bc663659ae8cc5ae3
rguerin.pro@gmail.com <rguerin.pro@gmail.com>
2020-09-28 16:59:33 +0200

    fix(parser): fix path resolution :bug:

--- functions/helpers.js
+++ functions/helpers.js
@@ -3,6 +3,6 @@ import path from 'path';
 const removeParentheses = (string) => string.replace(/[()]/g, '');
 const trimAll = (doubleArray = [[]]) => doubleArray.map((array) => array.map((elem) => elem.trim()));
 
-const revolvePath = (relativePath) => path.resolve('..', '..', '..', relativePath);
+const revolvePath = (relativePath) => path.resolve(relativePath);
 
 export { removeParentheses, trimAll, revolvePath };
